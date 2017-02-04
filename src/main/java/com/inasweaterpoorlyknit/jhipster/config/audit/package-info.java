a1cc2f5ad9458da341ff18bc30839dd92c2729ac
cahaskins <cahaskins@cpp.edu>
2017-02-04 14:13:11 -0800

    code generated by JHipster

---
+++ src/main/java/com/inasweaterpoorlyknit/jhipster/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.inasweaterpoorlyknit.jhipster.config.audit;
---
+++ src/main/java/com/inasweaterpoorlyknit/jhipster/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.inasweaterpoorlyknit.jhipster.repository;
+
+import com.inasweaterpoorlyknit.jhipster.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
